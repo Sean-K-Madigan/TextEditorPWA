9108ce8be35be97daf6ccbcde41b86cac4052faf
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-07-16 20:52:06 -0700

    new server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Serve static files from the React app build directory
+const staticPath = path.join(__dirname, '../client/dist');
+app.use(express.static(staticPath));
 
-require('./routes/htmlRoutes')(app);
+// Handle requests to the root
+app.get('*', (req, res) => {
+  const indexPath = path.join(staticPath, 'index.html');
+  if (fs.existsSync(indexPath)) {
+    res.sendFile(indexPath);
+  } else {
+    res.status(404).send('index.html not found');
+  }
+});
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
